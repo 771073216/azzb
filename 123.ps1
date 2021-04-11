7c56777904e8dd8e1ac0a664088ba42ad398be16
771073216 <37037387+771073216@users.noreply.github.com>
2021-04-11 23:31:15 +0800

    Add files via upload

--- 123.ps1
+++ 123.ps1
@@ -1,20 +1,49 @@
 $latest = (invoke-restmethod https://api.github.com/repos/XTLS/Xray-core/releases/latest | findstr tag_name).Split("v")[1]
 $local = (.\xray.exe -version).Split()[1]
-if ($local -lt $latest)
+$remotever = (invoke-restmethod https://api.github.com/repos/2dust/v2rayN/releases/latest | findstr tag_name).Split(":")[1].trim()
+$localver = (Get-Item .\v2rayN.exe).VersionInfo.FileVersion
+
+$kill = {
+$Key = 'HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings'
+$Name = 'ProxyEnable'
+$result = (Get-ItemProperty -Path "Registry::$Key").$Name
+$process = (Get-Process -ErrorAction SilentlyContinue -Name v2rayn)
+if ($process -ne $null)
+{
+  Get-Process -ErrorAction SilentlyContinue -Name v2rayN | Stop-Process
+}
+if ($result -eq 1)
+{
+  Set-ItemProperty -path Registry::$Key -Name $Name -value 0
+}
+}
+
+if ($local -eq $latest)
 {
   curl https://api.azzb.workers.dev/https://github.com/XTLS/Xray-core/releases/latest/download/Xray-windows-64.zip -O xray.zip
+}
+
+if ( $localver -eq $remotever )
+{
+  curl https://api.azzb.workers.dev/https://github.com/2dust/v2rayN/releases/latest/download/v2rayN.zip -O v2rayn.zip
+}
+
+if ($local -eq $latest)
+{
+  .$kill
   Expand-Archive -Path .\xray.zip -DestinationPath .\tmp ; del xray.zip
   Move-Item -Force -Path .\tmp\xray.exe -Destination .
 }
-$localver = (Get-Item .\v2rayN.exe).VersionInfo.FileVersion
-$remotever = (invoke-restmethod https://api.github.com/repos/2dust/v2rayN/releases/latest | findstr tag_name).Split(":")[1].trim()
-if ( $localver -lt $remotever )
+
+if ( $localver -eq $remotever )
 {
-  curl https://api.azzb.workers.dev/https://github.com/2dust/v2rayN/releases/latest/download/v2rayN.zip -O v2rayn.zip
+  .$kill
   Expand-Archive -Path .\v2rayn.zip -DestinationPath .\tmp ; del v2rayn.zip
   Move-Item -Force -Path .\tmp\v2rayN\zh-Hans\* -Destination .\zh-Hans ; del .\tmp\v2rayN\zh-Hans
   Move-Item -Force -Path .\tmp\v2rayN\* -Destination .
 }
+
 del -r -ErrorAction SilentlyContinue ./tmp/
+
 curl https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat -O geoip.dat
 curl https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -O geosite.dat
