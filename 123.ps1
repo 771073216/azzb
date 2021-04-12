cab8b6b7f48580464b1ed6d60c93be4298dc78d3
771073216 <37037387+771073216@users.noreply.github.com>
2021-04-12 11:37:11 +0800

    Add files via upload

--- 123.ps1
+++ 123.ps1
@@ -1,49 +1,57 @@
-$latest = (invoke-restmethod https://api.github.com/repos/XTLS/Xray-core/releases/latest | findstr tag_name).Split("v")[1]
-$local = (.\xray.exe -version).Split()[1]
-$remotever = (invoke-restmethod https://api.github.com/repos/2dust/v2rayN/releases/latest | findstr tag_name).Split(":")[1].trim()
-$localver = (Get-Item .\v2rayN.exe).VersionInfo.FileVersion
-
-$kill = {
-$Key = 'HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings'
-$Name = 'ProxyEnable'
-$result = (Get-ItemProperty -Path "Registry::$Key").$Name
-$process = (Get-Process -ErrorAction SilentlyContinue -Name v2rayn)
-if ($process -ne $null)
+function killprocess
 {
-  Get-Process -ErrorAction SilentlyContinue -Name v2rayN | Stop-Process
+	$Key = 'HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings'
+	$Name = 'ProxyEnable'
+	$result = (Get-ItemProperty -Path "Registry::$Key").$Name
+	$process = (Get-Process -ErrorAction SilentlyContinue -Name v2rayn)
+	if ($process -ne $null) { Get-Process -ErrorAction SilentlyContinue -Name v2rayN | Stop-Process }
+	if ($result -eq 1) { Set-ItemProperty -Path Registry::$Key -Name $Name -Value 0 }
 }
-if ($result -eq 1)
+
+function install
 {
-  Set-ItemProperty -path Registry::$Key -Name $Name -value 0
-}
+	if (Test-Path .\xray.zip)
+	{
+		killprocess
+		Expand-Archive -Path .\xray.zip -DestinationPath .\tmp; Remove-Item xray.zip
+		Move-Item -Force -Path .\tmp\xray.exe -Destination .
+	}
+
+	if (Test-Path .\v2rayn.zip)
+	{
+		killprocess
+		Expand-Archive -Path .\v2rayn.zip -DestinationPath .\tmp; Remove-Item v2rayn.zip
+		Move-Item -Force -Path .\tmp\v2rayN\zh-Hans\* -Destination .\zh-Hans; Remove-Item .\tmp\v2rayN\zh-Hans
+		Move-Item -Force -Path .\tmp\v2rayN\* -Destination .
+	}
 }
 
-if ($local -lt $latest)
+function update
 {
-  curl https://api.azzb.workers.dev/https://github.com/XTLS/Xray-core/releases/latest/download/Xray-windows-64.zip -O xray.zip
+	$latest = (Invoke-RestMethod https://api.github.com/repos/XTLS/Xray-core/releases/latest | findstr tag_name).Split("v")[1]
+	$local = (.\xray.exe -Version).Split()[1]
+	$remotever = (Invoke-RestMethod https://api.github.com/repos/2dust/v2rayN/releases/latest | findstr tag_name).Split(":")[1].trim()
+	$localver = (Get-Item .\v2rayN.exe).VersionInfo.FileVersion
+	if ($local -lt $latest)
+	{
+		curl https://api.azzb.workers.dev/https://github.com/XTLS/Xray-core/releases/latest/download/Xray-windows-64.zip -O xray.zip
+	}
+	if ($localver -lt $remotever)
+	{
+		curl https://api.azzb.workers.dev/https://github.com/2dust/v2rayN/releases/latest/download/v2rayN.zip -O v2rayn.zip
+	}
+	install
 }
 
-if ( $localver -lt $remotever )
+function installcore
 {
-  curl https://api.azzb.workers.dev/https://github.com/2dust/v2rayN/releases/latest/download/v2rayN.zip -O v2rayn.zip
+	curl https://api.azzb.workers.dev/https://github.com/XTLS/Xray-core/releases/latest/download/Xray-windows-64.zip -O xray.zip
+	Expand-Archive -Path .\xray.zip -DestinationPath .\tmp; Remove-Item xray.zip
+	Move-Item -Force -Path .\tmp\xray.exe -Destination .
 }
 
-if ($local -lt $latest)
-{
-  .$kill
-  Expand-Archive -Path .\xray.zip -DestinationPath .\tmp ; del xray.zip
-  Move-Item -Force -Path .\tmp\xray.exe -Destination .
-}
+if (Test-Path .\xray.exe) { update } else { installcore }
 
-if ( $localver -lt $remotever )
-{
-  .$kill
-  Expand-Archive -Path .\v2rayn.zip -DestinationPath .\tmp ; del v2rayn.zip
-  Move-Item -Force -Path .\tmp\v2rayN\zh-Hans\* -Destination .\zh-Hans ; del .\tmp\v2rayN\zh-Hans
-  Move-Item -Force -Path .\tmp\v2rayN\* -Destination .
-}
-
-del -r -ErrorAction SilentlyContinue ./tmp/
-
+Remove-Item -r -ErrorAction SilentlyContinue ./tmp/
 curl https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat -O geoip.dat
 curl https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -O geosite.dat
