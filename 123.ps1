73b35be40d82a4f659b30cd35ca38654b31ea071
771073216 <37037387+771073216@users.noreply.github.com>
2021-09-15 20:03:26 +0800

    Add files via upload

--- 123.ps1
+++ 123.ps1
@@ -1,11 +1,10 @@
 function killprocess
 {
 	$Key = 'HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings'
-	$Name = 'ProxyEnable'
-	$result = (Get-ItemProperty -Path "Registry::$Key").$Name
-	$process = (Get-Process -ErrorAction SilentlyContinue -Name v2rayn)
+	$result = (Get-ItemProperty "Registry::$Key").ProxyEnable
+	$process = (Get-Process -ErrorAction SilentlyContinue v2rayn)
 	if ($process -ne $null) { Stop-Process -Name v2rayN }
-	if ($result -eq 1) { Set-ItemProperty -Path Registry::$Key -Name $Name -Value 0 }
+	if ($result -eq 1) { Set-ItemProperty Registry::$Key ProxyEnable 0 }
 }
 
 if (!(Test-Path .\version))
@@ -15,46 +14,71 @@ if (!(Test-Path .\version))
 	"v2rayn:0" >> .\version
 }
 
-if (Test-Path .\tmp\) { Remove-Item -r .\tmp\ }
+if (Test-Path .\tmp\) { Remove-Item -Recurse .\tmp\ }
 if (Test-Path .\xray.zip) { Remove-Item xray.zip }
 if (Test-Path .\v2rayn.zip) { Remove-Item v2rayn.zip }
 
-$xraylatest = (Invoke-RestMethod https://cdn.jsdelivr.net/gh/771073216/dist@main/version | findstr xray).Split(":")[1].trim()
+curl https://cdn.jsdelivr.net/gh/771073216/dist@main/version -O remote.txt
+$xraylatest = (Get-Content .\remote.txt | findstr xray).Split(':')[1].trim()
 $xraylocal = (Get-Content .\version | findstr xray).Split(':')[1]
-$guilatest = (Invoke-RestMethod https://cdn.jsdelivr.net/gh/771073216/dist@main/version | findstr v2rayn).Split(":")[1].trim()
+$guilatest = (Get-Content .\remote.txt | findstr v2rayn).Split(':')[1].trim()
 $guilocal = (Get-Content .\version | findstr v2rayn).Split(':')[1]
 $geotime = Get-Date -Format 'yyM'
 $geolocal = (Get-Content .\version | findstr geo).Split(':')[1]
+Remove-Item remote.txt
 
 if ($xraylocal -lt $xraylatest)
 {
-	curl https://cdn.jsdelivr.net/gh/771073216/dist@main/xray-windows.zip -O xray.zip
+	curl https://cdn.jsdelivr.net/gh/771073216/dist@main/windows/xray-windows.zip -O xray.zip
 }
 if ($guilocal -lt $guilatest)
 {
-	curl https://cdn.jsdelivr.net/gh/771073216/dist@main/v2rayn.zip -O v2rayn.zip
+	curl https://cdn.jsdelivr.net/gh/771073216/dist@main/windows/v2rayn-v$guilatest.zip -O v2rayn.zip
 }
 if ($geotime -ne $geolocal)
 {
-	curl https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geoip.dat -O geoip.dat
-	curl https://cdn.jsdelivr.net/gh/Loyalsoldier/v2ray-rules-dat@release/geosite.dat -O geosite.dat
+	curl https://cdn.jsdelivr.net/gh/771073216/geofile@release/geoip.dat -O geoip.dat
+	curl https://cdn.jsdelivr.net/gh/771073216/geofile@release/geosite.dat -O geosite.dat
 }
 
 if (Test-Path .\xray.zip)
 {
 	killprocess
-	Expand-Archive -Path .\xray.zip -DestinationPath .\tmp; Remove-Item xray.zip
-	Move-Item -Force -Path .\tmp\xray.exe -Destination .
+	Expand-Archive .\xray.zip .\tmp
+	Remove-Item xray.zip
+	Move-Item -Force .\tmp\xray.exe .
 }
 if (Test-Path .\v2rayn.zip)
 {
 	killprocess
-	Expand-Archive -Path .\v2rayn.zip -DestinationPath .\tmp; Remove-Item v2rayn.zip
-	Move-Item -Force -Path .\tmp\v2rayN\zh-Hans\* -Destination .\zh-Hans; Remove-Item .\tmp\v2rayN\zh-Hans
-	Move-Item -Force -Path .\tmp\v2rayN\* -Destination .
+	Expand-Archive .\v2rayn.zip .\tmp
+	Remove-Item v2rayn.zip
+	if (Test-Path .\zh-Hans)
+	{ 
+		Remove-Item -Recurse .\zh-Hans
+		Move-Item -Force .\tmp\v2rayN\zh-Hans\ .
+	}
+	Move-Item -Force .\tmp\v2rayN\* .
 }
+
+if (Test-Path .\tmp\) {Remove-Item -Recurse .\tmp\ }
 
 "geo:$geotime" > .\version
 "xray:$xraylatest" >> .\version
 "v2rayn:$guilatest" >> .\version
-Remove-Item -r -ErrorAction SilentlyContinue .\tmp\
+
+if (!(Test-Path .\guiNConfig.json))
+{
+	.\v2rayN.exe
+	sleep 1
+	killprocess
+	$conf = Get-Content .\guiNConfig.json
+	clear-content .\guiNConfig.json
+	foreach ($line in $conf)
+	{
+		$liner = $line -replace '"coreType": 0,','"coreType": 1,' `
+		-replace '"routingIndex": 0,','"routingIndex": 1,' `
+		-replace '"enableRoutingAdvanced": false,','"enableRoutingAdvanced": true,'
+		Add-content .\guiNConfig.json $liner
+	}
+}
